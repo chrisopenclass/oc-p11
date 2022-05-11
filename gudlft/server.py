5aca89a70ec04795f7e7bfad8e252c4a42b96146
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-05-11 18:44:03 +0200

    correction d'un bug ou l'on pouvait acheter plus de place que des place disponible
    il n'est désormait plus possible d'acheter plus de place que des place disponible

--- gudlft/server.py
+++ gudlft/server.py
@@ -62,6 +62,9 @@ def purchasePlaces():
     if placesRequired > 12:
         flash("you can't buy more than 12 places!")
         return render_template("welcome.html", club=club, competitions=competitions)
+    if placesRequired > int(competition["numberOfPlaces"]):
+        flash("your trying to buy more places than avalaible places  !")
+        return render_template("welcome.html", club=club, competitions=competitions)
     competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
     flash("Great-booking complete!")
     return render_template("welcome.html", club=club, competitions=competitions)
