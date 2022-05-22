b378bfc90e0806df94341d941bbc81617a9cf654
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-05-22 17:55:25 +0200

    modification conftest
    modification des data du conftest pour avoir au moins un club avec 39 points ( pour les test d'achat de place supérieur à 12 )

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from gudlft.server import createapp
 
 
 DEFAULT_CLUBS = [
-    {"name": "Simply Lift", "email": "john@simplylift.co", "points": "13"},
+    {"name": "Simply Lift", "email": "john@simplylift.co", "points": "39"},
     {"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"},
     {"name": "She Lifts", "email": "kate@shelifts.co.uk", "points": "12"},
 ]
